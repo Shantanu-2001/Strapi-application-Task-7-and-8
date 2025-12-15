6130c332ac33e45c4bd643c7354cddc45f780a78
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2025-12-15 16:39:32 +0530

    Add ECS cluster, service, and task definition

---
+++ terraform/ecs.tf
@@ -0,0 +1,68 @@
+resource "aws_ecs_cluster" "strapi" {
+  name = "strapi-cluster"
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name   = "strapi-ecs-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_task_definition" "strapi" {
+  family                   = "strapi-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "512"
+  memory                   = "1024"
+
+  execution_role_arn = var.ecs_execution_role_arn
+  task_role_arn      = var.ecs_execution_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "strapi"
+      image = var.docker_image
+      portMappings = [
+        {
+          containerPort = 1337
+          hostPort      = 1337
+        }
+      ]
+      environment = [
+        { name = "DATABASE_CLIENT", value = "postgres" },
+        { name = "DATABASE_HOST", value = aws_db_instance.strapi_rds.address },
+        { name = "DATABASE_PORT", value = "5432" },
+        { name = "DATABASE_NAME", value = "strapi_db" },
+        { name = "DATABASE_USERNAME", value = "strapi" },
+        { name = "DATABASE_PASSWORD", value = "strapi123" }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "strapi" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.strapi.id
+  task_definition = aws_ecs_task_definition.strapi.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = data.aws_subnets.default.ids
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+}
