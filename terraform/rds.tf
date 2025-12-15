62b80c81696f3d1dfd72d7d58295f80a600e057a
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2025-12-15 22:28:12 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,18 +1,17 @@
+# -----------------------------
+# RDS Subnet Group
+# -----------------------------
 resource "aws_db_subnet_group" "strapi_db_subnet_group" {
   name       = "strapi-db-subnet-group-shantanu"
   subnet_ids = data.aws_subnets.default.ids
 }
 
+# -----------------------------
+# RDS Security Group
+# -----------------------------
 resource "aws_security_group" "strapi_rds_sg" {
   name   = "strapi-rds-sg"
   vpc_id = data.aws_vpc.default.id
-
-  ingress {
-    from_port                = 5432
-    to_port                  = 5432
-    protocol                 = "tcp"
-    source_security_group_id = aws_security_group.ecs_sg.id
-  }
 
   egress {
     from_port   = 0
@@ -22,6 +21,21 @@ resource "aws_security_group" "strapi_rds_sg" {
   }
 }
 
+# -----------------------------
+# Allow ECS → RDS (CORRECT WAY)
+# -----------------------------
+resource "aws_security_group_rule" "ecs_to_rds" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.strapi_rds_sg.id
+  source_security_group_id = aws_security_group.ecs_sg.id
+}
+
+# -----------------------------
+# RDS Instance
+# -----------------------------
 resource "aws_db_instance" "strapi_rds" {
   identifier          = "strapi-db"
   engine              = "postgres"
