a90c1e08c4399439f4b1a8fb562c4b00fce79724
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2025-12-15 20:35:53 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,8 +6,3 @@ variable "docker_image" {
   description = "ECR image pushed by CI"
   type        = string
 }
-
-variable "ecs_execution_role_arn" {
-  description = "Pre-existing ECS execution role ARN"
-  type        = string
-}
