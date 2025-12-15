28fbb8038110c1843556f7abdbd286ba7f875496
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2025-12-15 16:40:43 +0530

    Refactor variables in variables.tf
    Removed unused variables and updated descriptions.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,13 @@
 variable "aws_region" {
-  type        = string
-  default     = "ap-south-1"
+  default = "ap-south-1"
 }
- 
-variable "instance_type" {
-  type        = string
-  default     = "t2.micro"
-}
- 
-variable "key_name" {
-  description = "EC2 key pair name for SSH"
-  type        = string
-}
- 
+
 variable "docker_image" {
-  description = "Docker Hub image"
+  description = "ECR image pushed by CI"
   type        = string
 }
- 
-variable "strapi_port" {
-  type        = number
-  default     = 1337
-}
- 
-variable "allowed_ssh_cidr" {
+
+variable "ecs_execution_role_arn" {
+  description = "Pre-existing ECS execution role ARN"
   type        = string
-  default     = "0.0.0.0/0"
 }
