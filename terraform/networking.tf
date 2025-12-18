92f67c709906888e089dee0bd66bfc8f7a10b0ba
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2025-12-18 14:46:44 +0530

    Create networking.tf

---
+++ terraform/networking.tf
@@ -0,0 +1,29 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name   = "shantanu-strapi-ecs-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
